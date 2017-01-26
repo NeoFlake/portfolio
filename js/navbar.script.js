6b71537e0e5a3d8982d5390592d7270b4b6b3cd4
NeoFlake <jonathan.schmidt.13@gmail.com>
2017-01-26 10:27:12 +0100

    version 0.12 - commit obligatoire

---
+++ js/navbar.script.js
@@ -0,0 +1,24 @@
+$().ready(function(){
+
+	$("#firstNavButton").click(function(){
+			$("header a").removeClass("activatelink");
+			$("header a li").removeClass("bazarlink");
+			$("#firstNavButton").toggleClass("activatelink");
+			$("#firstNavButton li").toggleClass("bazarlink");
+	});
+
+	$("#secondNavButton").click(function(){
+			$("header a").removeClass("activatelink");
+			$("header a li").removeClass("bazarlink");
+			$("#secondNavButton").toggleClass("activatelink");
+			$("#secondNavButton li").toggleClass("bazarlink");
+	});
+
+	$("#thirdNavButton").click(function(){
+			$("header a").removeClass("activatelink");
+			$("header a li").removeClass("bazarlink");
+			$("#thirdNavButton").toggleClass("activatelink");
+			$("#thirdNavButton li").toggleClass("bazarlink");
+	});
+
+});
